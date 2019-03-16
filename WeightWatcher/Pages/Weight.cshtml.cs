baf0cfc4c9b93fd67612dfe0af8f222639f24af8
Nick Anderson <njcanderson@gmail.com>
2019-03-15 21:18:18 -0400

    fixed nav and added log weight button

---
+++ WeightWatcher/Pages/Weight.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeightWatcher.Pages
+{
+    public class WeightModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
